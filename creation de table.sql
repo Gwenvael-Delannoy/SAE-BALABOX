4568c096f4aaf1d0c0220e662319de6d4f835ae7
DamienMsn <105371569+DamienMsn@users.noreply.github.com>
2022-11-29 15:21:05 +0100

    Update creation de table.sql

--- creation de table.sql
+++ creation de table.sql
@@ -29,32 +29,32 @@ DOM_plongeons : {0,1}
 
 
 
-DROP TABLE SPORT;
-DROP TABLE EQUIPE;
-DROP TABLE SESSION;
-DROP TABLE ELEVE;
-DROP TABLE MATCH;
-DROP TABLE MATCH_EQUIPE;
-DROP TABLE MATCH_ELEVE;
+DROP TABLE Sport;
+DROP TABLE Equipe;
+DROP TABLE Session;
+DROP TABLE Eleve;
+DROP TABLE Match;
+DROP TABLE Match_Equipe;
+DROP TABLE Match_Eleve;
 
 
 
 
-CREATE TABLE SPORT(
+CREATE TABLE Sport(
     id_sport INTEGER    
         CONSTRAINT pk_Sport PRIMARY KEY,
     nom_sport TEXT NOT NULL,
     description_sport TEXT
 );
 
-CREATE TABLE EQUIPE(
+CREATE TABLE Equipe(
     id_equipe INTEGER    
         CONSTRAINT pk_Equipe PRIMARY KEY,
     nb_joueurs INTEGER,
     total INTEGER   
 );
 
-CREATE TABLE SESSION(
+CREATE TABLE Session(
     id_session INTEGER    
         CONSTRAINT pk_Session PRIMARY KEY,
     date_session TEXT NOT NULL,
@@ -65,10 +65,10 @@ CREATE TABLE SESSION(
     professeur TEXT NOT NULL,
     type_session TEXT NOT NULL,
     le_sport INTEGER 
-        CONSTRAINT fk_Session_Sport REFERENCES SPORT(id_sport)
+        CONSTRAINT fk_Session_Sport REFERENCES Sport(id_sport)
 );
 
-CREATE TABLE ELEVE(
+CREATE TABLE Eleve(
     id_eleve INTEGER    
         CONSTRAINT pk_Eleve PRIMARY KEY,
     nom TEXT NOT NULL,
@@ -77,31 +77,31 @@ CREATE TABLE ELEVE(
     classe TEXT NOT NULL,
     total_points INTEGER,   
     l_equipe INTEGER
-        CONSTRAINT fk_Eleve_Equipe REFERENCES EQUIPE(id_equipe)
+        CONSTRAINT fk_Eleve_Equipe REFERENCES Equipe(id_equipe)
 );
 
-CREATE TABLE MATCH(
+CREATE TABLE Match(
     id_match INTEGER    
         CONSTRAINT pk_Match PRIMARY KEY,
     resultat_equipe_1 INTEGER,
     resultat_equipe_2 INTEGER,
     la_session INTEGER
-        CONSTRAINT fk_Match_Session REFERENCES SESSION(id_session),    
+        CONSTRAINT fk_Match_Session REFERENCES Session(id_session),    
 );
 
-CREATE TABLE MATCH_EQUIPE(
+CREATE TABLE Match_Equipe(
     le_match INTEGER    
-        CONSTRAINT fk_Match_Equipe REFERENCES MATCH(id_match),
+        CONSTRAINT fk_Match_Equipe REFERENCES Match(id_match),
     les_equipes INTEGER    
-        CONSTRAINT fk_Equipe_Match REFERENCES EQUIPE(id_equipe),
+        CONSTRAINT fk_Equipe_Match REFERENCES Equipe(id_equipe),
     CONSTRAINT pk_Match_Equipe PRIMARY KEY (le_match, les_equipe)
 );
 
-CREATE MATCH_ELEVE(
+CREATE TABLE Match_Eleve(
     un_match INTEGER    
-        CONSTRAINT fk_Match_Eleve REFERENCES MATCH(id_match),
+        CONSTRAINT fk_Match_Eleve REFERENCES Match(id_match),
     les_eleves INTEGER    
-        CONSTRAINT fk_Eleve_Match REFERENCES ELEVE(id_eleve),
+        CONSTRAINT fk_Eleve_Match REFERENCES Eleve(id_eleve),
     CONSTRAINT pk_Match_Eleve PRIMARY KEY (id_match, id_eleve)
 );
 
