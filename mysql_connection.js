e4438da5838a146d2c33ca5f1e4d2468ab4d4a3d
TristanBourbigot <tristanbourbigot@gmail.com>
2022-12-10 15:49:56 +0100

    Update mysql_connection

--- mysql_connection.js
+++ mysql_connection.js
@@ -1,3 +1,37 @@
+const mysql = require('mysql');
+
 /**
  * MYSQL connection
  */
+class mysql_connection{
+
+    
+    /**
+     * Initialize and create the connection to the database
+     */
+    constructor(){
+        this.connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root', // nom de l'utilisateur de la base de donnee (a modifier si necessaire)
+            password: 'root' // mot de passe de l'utilisateur de la base de donnee (a modifier si necessaire)
+        });
+
+        this.connection.connect(function(err) {
+            if (err) throw err;
+            else console.log("Connecter a la bas de donnee");
+        });
+    }
+
+    /**
+     * getter of the connection to the database
+     * @returns {mysql_connection} connection to the database
+     */
+    getConnection(){
+        return this.connection;
+    }
+    
+
+}
+
+var db = new mysql_connection();
+module.exports = db.getConnection();
