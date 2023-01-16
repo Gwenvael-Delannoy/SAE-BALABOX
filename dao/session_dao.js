530fcc41491a7278fd8023cac418fcc4c1f9d7e2
TristanBourbigot <tristanbourbigot@gmail.com>
2023-01-16 12:39:11 +0100

    debug session dao

--- dao/session_dao.js
+++ dao/session_dao.js
@@ -15,7 +15,7 @@ var SessionDAO = function(){
      this.insert = function(session, callback){
         values = [session.getDate(), session.getStatut(), session.getHeure(), session.getIdentifiant(), session.getMdp(), session.getProfesseur(), session.getType(), session.getSport()];
         var sql = "INSERT INTO Session (date_session, statut, heure, identifiant_con, mdp, professeur, type_session, le_sport) VALUES ($date_session, $statut, $heure, $identifiant_con, $mdp, $professeur, $type_session, $le_sport)";
-        smt.run(sql, values ,callback);
+        smt.query(sql, values ,callback);
     };
 
     /**
@@ -26,8 +26,9 @@ var SessionDAO = function(){
      * @returns {void}
      */
     this.update = function(key, values, callback){
-        var sql2 = "UPDATE Session SET date_session=$date_session,statut=$statut,heure=$heure,identifiant_con=$identifiant_con,mdp=$mdp,professeur=$professeur,type_session=$type_session,le_sport=$le_sport WHERE id_session= " + key + ";";
-        smt.run(sql2,values, callback);
+        var sql2 = "UPDATE Session SET date_session=?,statut=?,heure=?,identifiant_con=?,mdp=?,professeur=?,type_session=?,le_sport=? WHERE id_session=?;";
+        values.push(key);
+        smt.query(sql2,values, callback);
     };
 
     /**
@@ -37,8 +38,8 @@ var SessionDAO = function(){
      * @returns {void}
      */
     this.delete = function(key, callback){
-        var sql3 = "DELETE FROM Session WHERE id_session= " + key + ";";
-        smt.run(sql3,callback);
+        var sql3 = "DELETE FROM Session WHERE id_session=?;";
+        smt.query(sql3,key,callback);
     };
 
     /**
@@ -48,7 +49,7 @@ var SessionDAO = function(){
      */
     this.findAll = function(callback){
         var sql4 = "SELECT * FROM Session";
-        smt.all(sql4, callback);
+        smt.query(sql4, callback);
     };
 
     /**
@@ -58,8 +59,8 @@ var SessionDAO = function(){
      * @returns {Session}
      */
     this.findByKey = function(key, callback){
-        var sql5 = "SELECT * FROM Session WHERE id_session = " + key + ";";
-        smt.get(sql5, callback);
+        var sql5 = "SELECT * FROM Session WHERE id_session =?;";
+        smt.query(sql5, key,callback);
     };
 };
 var sessionDAO = new SessionDAO();
