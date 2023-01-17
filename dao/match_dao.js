2c44cca9bfa5915f2c76d4ce7f1002724f335965
TristanBourbigot <tristanbourbigot@gmail.com>
2023-01-17 14:22:02 +0100

    update match

--- dao/match_dao.js
+++ dao/match_dao.js
@@ -13,8 +13,9 @@ var MatchDAO = function () {
      * @returns {void}
      */
     this.insert = function (match, callback) {
+        this.use(null);
         values = [match.getResultat1(), match.getResultat2(), match.getSession().getId()];
-        var sql = "INSERT INTO Match (resultat_equipe_1, resultat_equipe_2, la_session) VALUES ($resultat_equipe_1, $resultat_equipe_2, $la_session)";
+        var sql = "INSERT INTO Match (resultat_equipe_1, resultat_equipe_2, la_session) VALUES (?,?,?)";
         smt.run(sql, values, callback);
     };
 
@@ -26,7 +27,8 @@ var MatchDAO = function () {
      * @returns {void}
      */
     this.update = function (key, values, callback) {
-        var sql2 = "UPDATE Match SET resultat_equipe_1=$resultat_equipe_1,resultat_equipe_2=$resultat_equipe_2,la_session=$la_session WHERE id_match= " + key + ";";
+        this.use(null);
+        var sql2 = "UPDATE Match SET resultat_equipe_1=?,resultat_equipe_2=?,la_session=? WHERE id_match= " + key + ";";
         smt.run(sql2, values, callback);
     };
 
@@ -37,6 +39,7 @@ var MatchDAO = function () {
      * @returns {void}
      */
     this.delete = function (key, callback) {
+        this.use(null);
         var sql3 = "DELETE FROM Match WHERE id_match= " + key + ";";
         smt.run(sql3, callback);
     };
@@ -47,6 +50,7 @@ var MatchDAO = function () {
      * @returns {Match[]}
      */
     this.findAll = function (callback) {
+        this.use(null);
         var sql4 = "SELECT * FROM Match";
         smt.all(sql4, callback);
     };
@@ -58,6 +62,7 @@ var MatchDAO = function () {
      * @returns {Match}
      */
     this.findByKey = function (key, callback) {
+        this.use(null);
         var sql5 = "SELECT * FROM Match WHERE id_match= " + key + ";";
         smt.get(sql5, callback);
     };
@@ -69,9 +74,21 @@ var MatchDAO = function () {
      * @returns {Match[]}
      */
     this.findBySession = function (key, callback) {
+        this.use(null);
         var sql6 = "SELECT * FROM Match WHERE la_session= " + key + ";";
         smt.all(sql6, callback);
     };
+
+    /**
+     * Use the database balabox_sport_db
+     * @param {function} callback
+     * @returns {void}
+     */
+    this.use = function(callback){
+        var sql7 = "USE balabox_sport_db;";
+        smt.query(sql7, callback);
+    };
+    
 };
 var matchDAO = new MatchDAO();
 module.exports = matchDAO;
