88d785ee01311aa497e383ba97ca6b63fa9cb47e
Falckano <delannoy.e2100340@etud.univ-ubs.fr>
2023-01-18 16:08:50 +0100

    liste déroulante des prénom élèves v1

--- express_webapp/routes/classement_eleve.js
+++ express_webapp/routes/classement_eleve.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var eleve_dao = require('../models/dao/eleve_dao');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('classement_eleve');
+    var data = eleve_dao.findAll(function(err, rows, fields) {
+      
+        if (err) {
+            console.log(err);
+            res.render('error', {message: err.message, error: err});
+        } else {
+            res.render('classement_eleve', {title: 'Classement des élèves', eleves: rows});
+        }
+    });
 });
 
 module.exports = router;
