7e21dd7bd503bb91d1d767246173d552358615d0
Falckano <delannoy.e2100340@etud.univ-ubs.fr>
2023-01-19 14:25:29 +0100

    Update eleve_dao.js

--- express_webapp/routes/classement_eleve.js
+++ express_webapp/routes/classement_eleve.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var eleve_dao = require('../models/dao/dataBase').eleve_dao;
 
-/* GET home page. */
+/* GET home page. qui renvoie la page et donne un tableau de string avec les prenom des eleve */
 router.get('/', function(req, res, next) {
         var data1;
         eleve_dao.findName(function(err, rows) {
@@ -17,6 +17,22 @@ router.get('/', function(req, res, next) {
               res.render('classement_eleve', {eleves: data2});
           }
       });
+});
+
+
+router.post('/', function(req, res, next) {
+  var data1;
+  eleve_dao.findName(function(err, rows) {
+    if (err) {
+        console.log(err);
+        res.render('error', {message: err});
+    } else {
+        console.log(rows);
+        data1 = rows;
+        const data2 = data1.map(data1 => data1.prenom);
+        res.render('classement_eleve', {eleves: data2});
+    }
+});
 });
 
 module.exports = router;
