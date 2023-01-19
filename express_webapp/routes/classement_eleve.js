05ed0d09737c4be512b364cb80b537e340bbd670
Falckano <delannoy.e2100340@etud.univ-ubs.fr>
2023-01-19 14:42:38 +0100

    update classement eleve

--- express_webapp/routes/classement_eleve.js
+++ express_webapp/routes/classement_eleve.js
@@ -21,18 +21,8 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/', function(req, res, next) {
-  var data1;
-  eleve_dao.findName(function(err, rows) {
-    if (err) {
-        console.log(err);
-        res.render('error', {message: err});
-    } else {
-        console.log(rows);
-        data1 = rows;
-        const data2 = data1.map(data1 => data1.prenom);
-        res.render('classement_eleve', {eleves: data2});
-    }
+    render('classement_eleve');
 });
-});
+
 
 module.exports = router;
