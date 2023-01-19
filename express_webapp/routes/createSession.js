4f0c061a0a8cf41a7d77a2a50fe16e36a799eb24
TristanBourbigot <tristanbourbigot@gmail.com>
2023-01-19 16:31:31 +0100

    view create session

--- express_webapp/routes/createSession.js
+++ express_webapp/routes/createSession.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var sport_dao = require('../models/dao/dataBase').sport_dao;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('form_create_session');
+  var data1;
+  sport_dao.findAll(function(err, rows) {
+    if (err) console.log(err);
+    else {
+      data1 = rows;
+      const data2 = data1.map(data1 => data1.nom_sport);
+      res.render('form_create_session', {nom_sport: data2});
+    }
+  });
 });
 
 module.exports = router;
