b32d5f8f7dcda99d2534356f36fb68ec6ba6bdca
DamienMsn <105371569+DamienMsn@users.noreply.github.com>
2023-01-20 14:47:34 +0100

    liste Session presque fini

--- express_webapp/routes/listeSession.js
+++ express_webapp/routes/listeSession.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const session = require('express-session');
 var router = express.Router();
 var session_dao =  require('../models/dao/dataBase').session_dao;
 
@@ -17,27 +18,44 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
 
-  var id = req.body.delete;
+  var id = req.body.id;
+  console.log(id);
 
   session_dao.findByKey(id, function(err, row) {
-    console.log();
-    if(row[0].type_session== 'resultat'){
-      session_dao.deleteResultat(id, function(err) {
-        if (err) res.render("error", {message: err});
-        else {
-          console.log("delete");
-          res.redirect('listeSession');
-        }
-      });
+    if (err) res.render("error", {message: err});
+    else {
+      var id_sport = row[0].le_sport;
 
-    } else {
-      session_dao.deleteTournois(id, function(err) {
-        if (err) res.render("error", {message: err});
-        else {
-          console.log("delete");
-          res.redirect('listeSession');
-        }
-      });
+      if(req.body.action == 'update') {
+        res.redirect('crSession');
+    
+      } else if(req.body.action == 'view') {
+        res.redirect('/resultat?ses='+id+'&idSport='+id_sport+'');
+    
+      } else {
+    
+        session_dao.findByKey(id, function(err, row) {
+    
+          if(row[0].type_session== 'resultat'){
+            session_dao.deleteResultat(id, function(err) {
+              if (err) res.render("error", {message: err});
+              else {
+                console.log("delete");
+                res.redirect('listeSession');
+              }
+            });
+      
+          } else {
+            session_dao.deleteTournois(id, function(err) {
+              if (err) res.render("error", {message: err});
+              else {
+                console.log("delete");
+                res.redirect('listeSession');
+              }
+            });
+          }
+        });
+      }
     }
   });
 });
