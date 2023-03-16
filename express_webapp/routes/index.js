3a0e84c6171a96510fc2a958727334019a7abf4b
Meff7 <113592680+Meff7@users.noreply.github.com>
2023-03-16 18:44:37 +0100

    mise a jour de la page de co , fini

--- express_webapp/routes/index.js
+++ express_webapp/routes/index.js
@@ -12,7 +12,7 @@ var WebSocket = require('ws');
 router.get('/', function(req, res, next) {
 
   var message ='';
-  var professeur ='';
+  var professeur ='fzfz';
   //requeter l'api avec /authentified et on recuepre le role et on regarde si s'est un professer ou non
   /**
    var role = (appel api);
@@ -29,14 +29,13 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
 
-  var role = 4 //(appel api);
+  var role = 2 //(appel api);
   //dechiffrement du JWT TOKEN avec la clé public
 
   var ideCon= req.body.SIdentifiant;
   var pwd = req.body.Spwd;
   var messageError = '';
   var wss ;
-  console.log("ok");
   if(role == 4 ||role == 5 ){
     var nomEleve = req.body.SNomEleve;
     var prenomEleve = req.body.SPrenomEleve;
@@ -44,7 +43,6 @@ router.post('/', function(req, res, next) {
 
         // Connexion au websocket pour l eleve
         wss = new WebSocket('ws://localhost:3001');
-        console.log("ok2");
         wss.onopen = function () {
           console.log('Connexion websocket établie pour l eleve.');
         };
@@ -61,7 +59,6 @@ router.post('/', function(req, res, next) {
   }else if(role == 2 ||role == 3) {
     // Connexion au websocket pour le professeur
     wss = new WebSocket('ws://localhost:3002');
-    console.log("ok3");
     wss.onopen = function () {
       console.log('Connexion websocket établie pour le professeur.');
     };
@@ -75,64 +72,66 @@ router.post('/', function(req, res, next) {
     };
   }
 
-  //savoir sur quel bouton on a cliqué en renvoyant le nom du bouton
-  if(req.body.btn == 'BtConnexion'){
+  // Appeler la méthode de recherche de session
     
-    // Appeler la méthode de recherche de session
-    
-    session_dao.FindByIdCon(ideCon, function(err,rows) {
-      if (err ) {
-        messageError ='Connexion a la base de donnée impossible'
+  session_dao.FindByIdCon(ideCon, function(err,rows) {
+    if (err ) {
+      messageError ='Connexion a la base de donnée impossible'
+      res.render('index', { message: messageError });
+    }
+    else{
+      var session = rows;
+
+      if(session == null || session == undefined || session == ''){
+        messageError = 'Identifiant incorrect ou inexistant';
         res.render('index', { message: messageError });
       }
       else{
-        var session = rows;
 
-        if(session == null || session == undefined || session == ''){
-          messageError = 'Identifiant incorrect ou inexistant';
-          res.render('index', { message: messageError });
-        }
-        else{
+        //check si le mot de passe est correct
+        if(session[0].mdp == pwd){
 
-          if(session[0].mdp == pwd){
-            messageError = 'Connexion réussie';    
-            //en fonction de se qu'on recupere dans le process.env
+          //check si la session est en cours
+          if(session[0].statut == 'en cours'){
 
-            
+            //check si c'est un eleve ou un professeur
+            if( role == 2 || role == 3){
+              var nomEleve = "professeur";
+              var prenomEleve = "professeur";
+              var classeEleve = "professeur";
+              
+            }
 
-            if( role == 4 || role == 5){
-
-              //check si l'eleve existe deja dans la base de donnée
-              eleve_dao.findByName(nomEleve, function(err,rows) {
-                if (err ) {
-                  messageError ='Connexion a la base de donnée impossible'
+            //check si l'eleve ou le professeur(connecté en tant qu'eleve pour pouvoir etre aussi dans la session si besoin) existe deja dans la base de donnée
+            eleve_dao.findByName(nomEleve, function(err,rows) {
+              if (err ) {
+                messageError ='Connexion a la base de donnée impossible'
+              }
+              else{
+                var eleve_req = rows;
+                var prenomEleveBdd = '';
+        
+                //recupere le prenom de l'eleve de la base de donnée si il existe 
+                if(eleve_req.length != 0){
+                  prenomEleveBdd = eleve_req[0].prenom;
                 }
-                else{
-                  var eleve_req = rows;
-                  var prenomEleveBdd = '';
-          
-                  //recupere le prenom de l'eleve de la base de donnée si il existe 
-                  if(eleve_req.length != 0){
-                    prenomEleveBdd = eleve_req[0].prenom;
-                  }
-                  //check si l'eleve existe deja dans la base de donnée ou non meme s'il a le meme nom mais pas le meme prenom*
-                  if(eleve_req.length == 0 && prenomEleveBdd != prenomEleve){
-                    //si l'eleve n'existe pas on l'ajoute dans la base de donnée
-                    var eleve = new Eleve();
-                    //(nom, prenom, sexe, classe, total_points, equipe){
-                    eleve.init(nomEleve,prenomEleve,"homme",classeEleve,0,1);
-                    eleve_dao.insert(eleve, function(err,rows) {
-                      if (err ) {
-                        messageError =err;
-                        console.log(messageError);
-                      }
-                      else{
-                        console.log('nouveau eleve cree');
+                //check si l'eleve existe deja dans la base de donnée ou non meme s'il a le meme nom mais pas le meme prenom*
+                if(eleve_req.length == 0 && prenomEleveBdd != prenomEleve){
+                  //si l'eleve n'existe pas on l'ajoute dans la base de donnée
+                  var eleve = new Eleve();
+                  //(nom, prenom, sexe, classe, total_points, equipe){
+                  eleve.init(nomEleve,prenomEleve,"homme",classeEleve,0,1);
+                  eleve_dao.insert(eleve, function(err,rows) {
+                    if (err ) {
+                      messageError =err;
+                      console.log(messageError);
+                    }
+                    else{
+                      console.log('nouveau eleve/professeur connecter en tant qu eleve cree ');
 
-                      }
-                    });
-                  }
-                  console.log("ok5");
+                    }
+                  });
+
                   // Envoi d'un message vers le serveur WebSocket de l'élève
                   wss.send(JSON.stringify({
                     type: 'info_eleve',
@@ -142,8 +141,8 @@ router.post('/', function(req, res, next) {
                     session: session[0].id_session
                   }));
                 }
-              });
-            }
+              }
+            });
               
             //renvoie la page en fonction du type de session
             if(session[0].type_session == 'tournoi equipe'){
@@ -155,15 +154,15 @@ router.post('/', function(req, res, next) {
             }
           }
           else{
-            messageError = 'Mot de passe incorrect';
-            res.render('index', { message: messageError });
+            messageError = 'Session terminée';
+            res.render('error', { message: messageError });
           }
+        }else{
+          messageError = 'Mot de passe incorrect';
+          res.render('error', { message: messageError });
         }
       }
-    });
-  }
-  else if(req.body.btn == 'BtAccesProf'){
-    res.redirect('/listeSession');
-  }
+    }
+  });
 });
 module.exports = router;
