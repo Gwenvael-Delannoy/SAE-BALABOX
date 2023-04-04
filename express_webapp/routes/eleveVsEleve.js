2bc06d190df57fd0f78aead37757177b3f72cf7c
TristanBourbigot <tristanbourbigot@gmail.com>
2023-04-04 13:47:56 +0200

    update match eleve

--- express_webapp/routes/eleveVsEleve.js
+++ express_webapp/routes/eleveVsEleve.js
@@ -5,12 +5,22 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     var idSession = req.query.idSession;
     var NomAdversaire = req.query.NomAdversaire;
-    res.render('eleve_contre_eleve', {idSession : idSession, NomAdversaire : NomAdversaire});
+    var idEleCo = req.query.idEleCo;
+    res.render('eleve_contre_eleve', {idSession : idSession, NomAdversaire : NomAdversaire, idEleCo : idEleCo});
 });
 
 router.post('/', function(req, res, next) {
-    console.log(req.body.score1);
-    res.render('eleve_contre_eleve');
+    score1 = req.body.score1;
+    score2 = req.body.score2;
+    idSession = req.body.idSession;
+    NomAdversaire = req.body.NomAdversaire;
+    console.log("Score 1 : " + score1);
+    console.log("Score 2 : " + score2);
+    console.log("idSession : " + idSession);
+    console.log("NomAdversaire : " + NomAdversaire);
+
+    
+    res.render('eleve_contre_eleve',{idSession : idSession, NomAdversaire : NomAdversaire});
 });
 
 module.exports = router;
