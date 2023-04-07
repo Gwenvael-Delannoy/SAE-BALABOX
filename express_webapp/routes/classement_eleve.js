562f0de4bfb12ef70c71fa87ecdda05539d007ae
Falckano <delannoy.e2100340@etud.univ-ubs.fr>
2023-04-07 16:20:10 +0200

    affiche pas son nom

--- express_webapp/routes/classement_eleve.js
+++ express_webapp/routes/classement_eleve.js
@@ -15,9 +15,7 @@ router.get('/', function(req, res,next) {
     idSession = req.query.ses;
     idEleCo = req.query.id_el;
     console.log("idEleCo :" +idEleCo);
-    eleveCo = [];
 
-    /**
     eleve_dao.findByKey(idEleCo, function(err,rows) {
         if (err ) {
             messageError ='Connexion à la base de donnée impossible';
@@ -25,13 +23,26 @@ router.get('/', function(req, res,next) {
         }
         else{
             
-            eleveCo[0] = rows[0].nom;
-            eleveCo[1] = rows[0].prenom;
-            console.log("eleveCo :" +eleveCo);
+            nomCo = rows[0].nom;
+            prenomCo = rows[0].prenom;
+
+            if (typeof nomCo !== 'string') {
+                 nomCo = String(nomCo);
+            }
+        
+            if (typeof prenomCo !== 'string') {
+                 prenomCo = String(prenomCo);
+            }
+            console.log("nomCo :" +nomCo);
+            console.log("prenomCo :" +prenomCo);
+
+
+            
         }
     }); 
     
-    */
+    
+
     
     
     match_dao.findAllMatchSes(idSession, function(err,rows) {
@@ -76,7 +87,7 @@ router.get('/', function(req, res,next) {
                                     }
                                 }
                                 console.log("ok");
-                                if(trouver == 'false'){
+                                if(trouver == 'false'){ //si il n'est pas dans le tableau
                                     eleves_res[0] = id_l_eleve;
 
                                     eleve_dao.findByKey(id_l_eleve, function(err,rows) {
@@ -85,22 +96,24 @@ router.get('/', function(req, res,next) {
                                             res.render('error',{message : messageError});
                                         }
                                         else{
-                                            eleves_res[1] = rows[0].prenom;
+                                            console.log('rows nom :' +rows.nom);
+                                            var el = rows;
+                                            eleves_res[1] = rows[0].nom; // nom de l'eleve
                                             console.log("eleve pres:" +rows[0].nom);
-                                            eleves_res[2] = rows[0].prenom;
+                                            eleves_res[2] = rows[0].prenom; // prenom de l'eleve
                                             console.log("eleve pres:" +rows[0].prenom);
                                         }
                                     });
-                                    eleves_res[3] = rows[i].gagnant;
-                                    eleves_res[4] = 1;
+                                    eleves_res[3] = rows[i].gagnant; // nombre de point de l'eleve
+                                    eleves_res[4] = 1;  // nombre de match de l'eleve
                                     console.log(eleves_res);
                                     classement.push(eleves_res);
                                     console.log("classement :" +classement);
                                     console.log("ok1");
                                 }
                                 else{
-                                    classement[indexTrouver][3] = classement[indexTrouver][3] + rows[i].gagnant;
-                                    classement[indexTrouver][4] = classement[indexTrouver][4] + 1;
+                                    classement[indexTrouver][3] = classement[indexTrouver][3] + rows[i].gagnant; // nombre de point de l'eleve
+                                    classement[indexTrouver][4] = classement[indexTrouver][4] + 1; // nombre de match de l'eleve
                                     console.log("classement :" +classement);
                                     console.log("ok2");
                                 }
@@ -109,18 +122,10 @@ router.get('/', function(req, res,next) {
                              
                         }
                     });
-
-                    nomCo = classement[classement.length - 1].nom;
-                    prenomCo = classement[classement.length - 1].prenom;
-
-
-                    
-                    
-
                 }
             }
 
-            res.render('classement_eleve', { idSession : idSession, classement : classement, eleveCo : eleveCo});
+            res.render('classement_eleve', { idSession : idSession, classement : classement, nomCo : nomCo, prenomCo : prenomCo});
         }
     });
 
@@ -148,7 +153,7 @@ router.post('/', function(req, res, next) {
     if(NomAdversaire == ''){
         res.render('error',{message : "Veuillez choisir un adversaire"});
     }else{
-        res.render('eleve_contre_eleve', { idSession : idSession, NomAdversaire : NomAdversaire, idEleCo : idEleCo, nomCo : nomCo, prenomCo : prenomCo});
+        res.render('eleve_contre_eleve', { idSession : idSession, NomAdversaire : NomAdversaire, idEleCo : idEleCo});
         console.log("idSession :" +idSession+ "\nNomAdversaire :" +NomAdversaire);
     }
     
