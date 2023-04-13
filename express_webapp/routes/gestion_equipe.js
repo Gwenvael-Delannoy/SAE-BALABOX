68ac5c995a0b297163dcf5702f9e5f11389299e8
TristanBourbigot <tristanbourbigot@gmail.com>
2023-04-14 01:22:47 +0200

    ajout equipe

--- express_webapp/routes/gestion_equipe.js
+++ express_webapp/routes/gestion_equipe.js
@@ -5,6 +5,9 @@ var equipe_dao =  require('../models/dao/dataBase').equipe_dao;
 var sport_dao = require('../models/dao/dataBase').sport_dao;
 var eleve_dao = require('../models/dao/dataBase').eleve_dao;
 var session_dao = require('../models/dao/dataBase').session_dao;
+var match_dao = require('../models/dao/dataBase').match_dao;
+var match = require('../models/match');
+var Equipe = require('../models/Equipe');
 
 
 
@@ -12,8 +15,7 @@ var session_dao = require('../models/dao/dataBase').session_dao;
 router.get('/', function(req, res, next) {
   var idSession = req.query.idsession;
   var type = req.query.type;
-  console.log(type)
-  equipe_dao.findEquipeSession(idSession, function(err,rows1) {
+  equipe_dao.findAll(function(err,rows1) {
     if (err ) {
         messageError ='Session inexistante,merci de revenir en arriere et ressayer';
         res.render('error',{message : messageError});
@@ -22,7 +24,7 @@ router.get('/', function(req, res, next) {
       var equipes = [];
       
 
-      equipe_dao.findAllEquipeEleve( function(err,rows2) {
+      equipe_dao.findAllEquipeEleve(function(err,rows2) {
         if (err ) {
             messageError ='Session inexistante,merci de revenir en arriere et ressayer';
             res.render('error',{message : messageError});
@@ -36,16 +38,8 @@ router.get('/', function(req, res, next) {
                 }
               }
               equipes.push({id_equipe : rows1[i].id_equipe, eleves : eleves});
+              
           }
-          for(var i = 0; i < equipes.length; i++){
-            var id = equipes[i].id_equipe;
-            for(var j = 0; j < equipes.length; j++){
-              if(id == equipes[j].id_equipe && i != j){
-                equipes.splice(j);
-              }
-            }
-          }
-          // console.log(equipes);
           res.render('gestion_equipe',{idSession : idSession, type : type, equipes : equipes});
         }
       });
@@ -55,13 +49,63 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
+  var statut = req.body.statut;
 
-      // Récupérer les données envoyées par le formulaire
-        var nom_equipe = req.body.nom;
-        var elevesEquipe = req.body.membres;
-        console.log(nom_equipe);
-    
-    // Envoyer une réponse au client
-    res.status(200).json({ message: 'Équipe ajoutée avec succès.' });
+  if(statut == "newEquipe"){
+    var idSession = req.body.idSession;
+
+    var equipe = new Equipe();
+    equipe.init(0,0);
+    equipe_dao.insert(equipe, function(err) {
+      if (err ) {
+          messageError ='Session inexistante,merci de revenir en arriere et ressayer';
+          res.render('error',{message : messageError});
+      }
+      else{
+        console.log("equipe inseree");
+        equipe_dao.findAll(function(err,rows) {
+          if(err){
+            messageError ='Session inexistante,merci de revenir en arriere et ressayer';
+            res.render('error',{message : messageError});
+          }else{
+            equipe_dao.findAll(function(err,rows1) {
+              if (err ) {
+                  messageError ='Session inexistante,merci de revenir en arriere et ressayer';
+                  res.render('error',{message : messageError});
+              }
+              else{
+                var equipes = [];
+                
+          
+                equipe_dao.findAllEquipeEleve(function(err,rows2) {
+                  if (err ) {
+                      messageError ='Session inexistante,merci de revenir en arriere et ressayer';
+                      res.render('error',{message : messageError});
+                  }
+                  else{
+                    for(var i = 0; i < rows1.length; i++){
+                      var eleves = [];
+                        for(var j = 0; j < rows2.length; j++){
+                          if(rows1[i].id_equipe == rows2[j].id_equipe){
+                            eleves.push({id_eleve : rows2[j].id_eleve, nom : rows2[j].nom, prenom : rows2[j].prenom});
+                          }
+                        }
+                        equipes.push({id_equipe : rows1[i].id_equipe, eleves : eleves});
+                        
+                    }
+                    res.render('gestion_equipe',{idSession : idSession, type : "tournoi equipe", equipes : equipes});
+                  }
+                });
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+
+
+  
 });
-  module.exports = router;
+
+module.exports = router;
