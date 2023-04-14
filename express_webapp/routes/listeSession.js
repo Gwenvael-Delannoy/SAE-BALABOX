37830461287ca05477cc888c79a4a7241c6c0eb6
DamienMsn <105371569+DamienMsn@users.noreply.github.com>
2023-04-14 10:44:23 +0200

    Update listeSession.js

--- express_webapp/routes/listeSession.js
+++ express_webapp/routes/listeSession.js
@@ -40,7 +40,7 @@ router.get('/', function(req, res, next) {
       res.render("error", {message: err});
     }
     else{
-      res.render('listeSession?prof='+nom_prof+'',{session : rows});
+      res.render('listeSession',{session : rows});
     }
   });
 });
